a5c67bc56de3b9e4024ba7379395e0dd8ebdea0b
xuanyue_18 <1820921675@qq.com>
2023-09-04 22:22:16 +0800

    页面布局、通用导航栏组件、全局状态、权限管理开发

---
+++ src/layouts/BasicLayout.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="basicLayout">
+    <a-layout style="height: 100vh">
+      <a-layout-header class="header">
+        <global-header />
+      </a-layout-header>
+      <a-layout-content class="content">
+        <router-view />
+      </a-layout-content>
+      <a-layout-footer class="footer">
+        <a href="https://github.com/xuanyue18"
+          >Xcode Online Judge by xuanyue18</a
+        >
+      </a-layout-footer>
+    </a-layout>
+  </div>
+</template>
+
+<style>
+#basicLayout {
+}
+
+#basicLayout .header {
+  margin-bottom: 16px;
+  box-shadow: #eee 1px 1px 5px;
+}
+
+#basicLayout .content {
+  background: linear-gradient(to right, #0b6dce, #27a68f);
+  margin-bottom: 16px;
+  padding: 20px;
+}
+
+#basicLayout .footer {
+  background: #42b983;
+  padding: 16px;
+  position: sticky;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  text-align: center;
+}
+</style>
+<script>
+import GlobalHeader from "@/components/GlobalHeader";
+
+export default {
+  components: { GlobalHeader },
+};
+</script>
